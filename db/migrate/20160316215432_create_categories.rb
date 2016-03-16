a55bd9c1f050a65a430c5b169a5bb4d8a417632b
xGioe <giovanni@mriva.me>
2016-03-16 22:59:20 +0100

    Generate models

---
+++ db/migrate/20160316215432_create_categories.rb
@@ -0,0 +1,11 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :cat_name
+      t.decimal :tax_fee, precision: 5, scale: 2
+      t.boolean :fee_free
+
+      t.timestamps null: false
+    end
+  end
+end
