1449bbbc38dcf1230eddca558078bb0e41ff5a6f
xGioe <giovanni@mriva.me>
2016-03-18 17:27:15 +0100

    Fix setup in products test. Finish products test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ActiveRecord::Base
   belongs_to :category
 
   validates :name, presence: true, length: { minimum: 3 }
-  validates :price, presence: true
-  validates :imported, presence: true
-  validates :category, presence: true
-  validates :qnty, presence: true, length: { minimum: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :imported, inclusion: { in: [true, false] }
+  validates :category_id, presence: true
+  validates :qnty, presence: true, numericality: { greater_than: 0 }
 
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,12 +3,44 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
 
   def setup
-    @category = Category.new(cat_name: 'food', fee_free: true)
-    @product = Product.new(name: 'banana', price: '10.0', imported: false, category: @category, qnty: 1)
+    category_id = 1
+    @product = Product.new(name: 'banana', price: '10.0', imported: false, category_id: category_id, qnty: 1)
   end
 
   test 'should be valid' do
+    assert @product.valid?
+  end
+
+  test 'name should be present' do
+    @product.name = "   "
     assert_not @product.valid?
   end
+
+  test 'name should not have less than three characters' do
+    @product.name = "a" * 2
+    assert_not @product.valid?
+  end
+
+  test 'price should be at least zero' do
+    @product.price = -3
+    assert_not @product.valid?
+  end
+
+  test 'imported should be specified' do
+    @product.imported = nil
+    assert_not @product.valid?
+  end
+
+  test 'category_id should be present' do
+    @product.category_id = nil
+    assert_not @product.valid?
+  end
+
+  test 'qnty should be atleast 1' do
+    @product.qnty = 0
+    assert_not @product.valid?
+  end
+
+
 
 end
